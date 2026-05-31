e4843524fb7fb28dee25c9fc05b87d2f80b0b3c1
Emrullah Yildiz <emrullahyildizarch@gmail.com>
2026-05-31 17:06:51 +0300

    update

--- integrations/revit-addin/obj/Debug/net10.0-windows/Nova.RevitAddin.AssemblyInfo.cs
+++ integrations/revit-addin/obj/Debug/net10.0-windows/Nova.RevitAddin.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Nova.RevitAddin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+096a17cc22c2769e895eac7994048817b3ad061b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cd9b606d6e00df5eb3b98ea39d7109c9fe2faf0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Nova.RevitAddin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Nova.RevitAddin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
